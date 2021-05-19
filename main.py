234a12de260d634a2289219595711a4297cc2db0
khai9xht <hoangnv@DESKTOP-EQGV9QP.localdomain>
2021-05-19 14:55:45 +0700

    test wandb0

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def main():
 
     model = Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
+    wandb.config.model_parameters = model.parameters()
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
