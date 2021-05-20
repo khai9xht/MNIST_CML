c334313c64ccbc5f69338b6277d6ea8828c41567
khai9xht <hoangnv@DESKTOP-EQGV9QP.localdomain>
2021-05-20 11:14:39 +0700

    add tqdm

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 import wandb
+from tqdm import tqdm
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -36,20 +38,28 @@ class Net(nn.Module):
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        wandb.log({'NLL_loss': loss.item()})
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
+    with tqdm(
+        total=CONFIG["num epochs"], \
+        desc=f"{epoch}/{CONFIG["num epochs"]}", \
+        postfix=dict, mininterval=0.1
+    ) as pbar:
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            wandb.log({'NLL_loss': loss.item()})
+            loss.backward()
+            optimizer.step()
+            if batch_idx % args.log_interval == 0:
+                pbar.set_postfix(**{
+                    "train Epoch": epoch,
+                    "step": batch_idx * len(data) / len(train_loader.dataset)
+                    "Loss": loss.item()
+                })
+                pbar.update(1)
+                if args.dry_run:
+                    break
 
 
 def test(model, device, test_loader):
